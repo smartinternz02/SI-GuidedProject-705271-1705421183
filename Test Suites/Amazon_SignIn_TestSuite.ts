705c03f8528b5d896bcb93b79a1a6e4dd4e01f82
akash <akash@LAPTOP-GQIN8V4C>
2024-01-31 21:30:17 +0530

    Adding test suites

---
+++ Test Suites/Amazon_SignIn_TestSuite.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Amazon_SignIn_TestSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>2636f637-3ca8-4593-b5d0-271be8785118</testSuiteGuid>
+   <testCaseLink>
+      <guid>d0c01e49-76b1-48b5-b4cf-3ebeca3fbb65</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Validation_Amazon_SignIn_By_Madhu</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
